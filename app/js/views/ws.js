41341fadb3790cd76ce8c0b57b995f844745b767
santi <santi@proper-code.com>
2017-12-17 01:36:47 -0800

    ws url

--- app/js/views/ws.js
+++ app/js/views/ws.js
@@ -1,6 +1,6 @@
 var Ws = function( parent ) {
 	this.parent = parent;
-	this.host = ( location.origin.indexOf( 'localhost' ) !== -1 ) ? 'ws://localhost:8080' : 'ws://54.93.229.12ws://ec2-54-93-229-125.eu-central-1.compute.amazonaws.com:80805'; // host IP
+	this.host = ( location.origin.indexOf( 'localhost' ) !== -1 ) ? 'ws://localhost:8080' : 'ws://54.93.229.12ws://ec2-54-93-229-125.eu-central-1.compute.amazonaws.com:8080'; // host IP
 
 
 	// this.socket = new WebSocket( this.host );
